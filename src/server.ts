2ee38cdb2c3decd0ff71f29568574b923879e442
Joshua Engel <engel@purgo.net>
2020-11-01 17:07:55 -0500

    Unify client and back-end server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
 import game from './game'
+import path from 'path';
+import serve from 'koa-static';
 const { Server } = require('boardgame.io/server');
 
 const server = Server({ games: [game] });
 
-server.run(8000);
+const PORT = process.env.PORT || 8000;
+
+// Build path relative to the server.js file
+const frontEndAppBuildPath = path.resolve(__dirname, '../build');
+server.app.use(serve(frontEndAppBuildPath))
+
+server.run(PORT, () => {
+    server.app.use(
+        async (ctx : any, next : any) => await serve(frontEndAppBuildPath)(
+            Object.assign(ctx, { path: 'index.html' }),
+            next
+        )
+    )
+});

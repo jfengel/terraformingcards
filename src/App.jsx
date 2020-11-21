33e8460b550e45cbf54e55d9268084c05506c528
Joshua Engel <engel@purgo.net>
2020-11-21 18:03:30 -0500

    Support for creating game URLs

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,32 @@ import game from './game';
 import board from './components/Board'
 import React from "react";
 
-import { Lobby } from 'boardgame.io/react';
+import {Lobby} from 'boardgame.io/react';
+import {Client} from "boardgame.io/react";
+import {SocketIO} from "boardgame.io/multiplayer";
 
 const {protocol, hostname, port} = window.location;
 
 const gameServer = `${protocol}//${hostname}:${port}`
 
-export default () => <Lobby
-    gameServer={gameServer}
-    lobbyServer={gameServer}
-    gameComponents={[
-        { game, board }
-    ]}
-/>;
+const urlParams = new URLSearchParams(window.location.search);
+const playerID = urlParams.get('playerID');
+const matchID = urlParams.get('matchID');
+const credentials = urlParams.get('credentials');
+
+const play = () => {
+    const multiplayer = SocketIO({server: gameServer});
+    const TMClient = Client({game, board, multiplayer})
+    return <TMClient {...{playerID, matchID, credentials}}/>
+}
+
+export default
+() => playerID && matchID ? play() :
+    <Lobby
+
+        gameServer={gameServer}
+        lobbyServer={gameServer}
+        gameComponents={[
+            {game, board}
+        ]}
+    />;
